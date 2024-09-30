e4cda02d0015691797aaf52481faa63257afc20d
eusuntbuga <bugam5160@gmail.com>
2024-09-30 15:22:18 +0300

    Update and rename game_engine.hpp to gameengine.cpp

---
+++ Pacman-main/gameengine.cpp
@@ -0,0 +1,22 @@
+#include "gameengine.hpp"
+
+GameEngine::GameEngine() : _pacman(), _board() {
+    // Inițializare fantome
+}
+
+void GameEngine::Init() {
+    // Inițializarea jocului
+}
+
+void GameEngine::Run() {
+    // Loop-ul principal al jocului
+}
+
+bool GameEngine::CheckCollision() {
+    // Verificare coliziune
+    return false;
+}
+
+void GameEngine::Display() {
+    // Afișare stare joc
+}
