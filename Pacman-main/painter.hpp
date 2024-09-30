84abef77083a58a21a0ed135ed409d3b9225f139
eusuntbuga <bugam5160@gmail.com>
2024-09-30 15:54:06 +0300

    Create painter.hpp

---
+++ Pacman-main/painter.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "point.hpp"
+
+class Painter {
+public:
+   void DrawImage(Point topLeft, Point bottomRight, char** image);
+   void WriteText(Point position, char* text);
+};
