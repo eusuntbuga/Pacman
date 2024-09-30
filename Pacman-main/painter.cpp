03ca6e68fbb7144abf8b3ef2a6279382ea768ba5
eusuntbuga <bugam5160@gmail.com>
2024-09-30 15:22:44 +0300

    Update and rename painter.hpp to painter.cpp

---
+++ Pacman-main/painter.cpp
@@ -0,0 +1,9 @@
+#include "painter.hpp"
+
+void Painter::DrawImage(Point topLeft, Point bottomRight, char** image) {
+    // Implementare desenare imagine
+}
+
+void Painter::WriteText(Point position, char* text) {
+    // Implementare scriere text
+}
