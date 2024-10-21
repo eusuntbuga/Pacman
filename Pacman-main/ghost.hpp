981337ef8952bccdde2b372449938b974588bbff
eusuntbuga <bugam5160@gmail.com>
2024-10-21 15:48:23 +0300

    Update ghost.hpp

--- Pacman-main/ghost.hpp
+++ Pacman-main/ghost.hpp
@@ -5,8 +5,22 @@ class Ghost {
    Point _position;
    char _color;
 public:
-   Ghost(const Point& position, char color);
+   // Constructor implicit
+   Ghost(const Point& position = {0, 0}, char color = 'W');
+
+   // Constructor de copiere
+   Ghost(const Ghost& other);
+
+   // Operator de copiere
+   Ghost& operator=(const Ghost& other);
+
+   // Operator de comparație
+   bool operator==(const Ghost& other) const;
+
    Point GetPosition() const;
    void Move();
    char GetColor() const;
+
+   // Operator de afișare
+   friend std::ostream& operator<<(std::ostream& os, const Ghost& ghost);
 };
