c36a03b3a610b58eca96f4dd529e80471c25dd36
eusuntbuga <bugam5160@gmail.com>
2024-10-21 15:49:40 +0300

    Update point.hpp

--- Pacman-main/point.hpp
+++ Pacman-main/point.hpp
@@ -3,4 +3,15 @@
 struct Point {
    int x;
    int y;
+
+   // Operator de comparație
+   bool operator==(const Point& other) const {
+       return x == other.x && y == other.y;
+   }
+
+   // Operator de afișare
+   friend std::ostream& operator<<(std::ostream& os, const Point& point) {
+       os << "(" << point.x << ", " << point.y << ")";
+       return os;
+   }
 };
