dd9a935f696f83e244a7a230412e81d7fe4ab2ef
eusuntbuga <bugam5160@gmail.com>
2024-10-21 15:47:29 +0300

    Update board.hpp

--- Pacman-main/board.hpp
+++ Pacman-main/board.hpp
@@ -4,7 +4,21 @@ class Board {
    int _width;
    int _height;
 public:
+   // Constructor implicit
    Board(int width = 28, int height = 31);
+
+   // Constructor de copiere
+   Board(const Board& other);
+
+   // Operator de copiere
+   Board& operator=(const Board& other);
+
+   // Operator de comparație
+   bool operator==(const Board& other) const;
+
    int GetWidth() const;
    int GetHeight() const;
+
+   // Operator de afișare
+   friend std::ostream& operator<<(std::ostream& os, const Board& board);
 };
