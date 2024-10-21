3d9262f92415d1015d7385adb6bea00d5a5ac589
eusuntbuga <bugam5160@gmail.com>
2024-10-21 15:49:02 +0300

    Update pacman.hpp

--- Pacman-main/pacman.hpp
+++ Pacman-main/pacman.hpp
@@ -5,10 +5,26 @@ class Pacman {
    Point _position;
    int _lives;
 public:
+   // Constructor implicit
    Pacman();
+
+   // Constructor cu parametri
    Pacman(const Point& position, int lives);
+
+   // Constructor de copiere
+   Pacman(const Pacman& other);
+
+   // Operator de copiere
+   Pacman& operator=(const Pacman& other);
+
+   // Operator de comparație
+   bool operator==(const Pacman& other) const;
+
    Point GetPosition() const;
    void Move(char direction);
    int GetLives() const;
    void LoseLife();
+
+   // Operator de afișare
+   friend std::ostream& operator<<(std::ostream& os, const Pacman& pacman);
 };
