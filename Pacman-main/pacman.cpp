43c5f703f403a026aee88e6d403c744ac9c6e095
eusuntbuga <bugam5160@gmail.com>
2024-11-10 13:01:17 +0200

    Update pacman.cpp

--- Pacman-main/pacman.cpp
+++ Pacman-main/pacman.cpp
@@ -3,6 +3,15 @@
 
 Pacman::Pacman() : _position{0, 0}, _lives(3) {}
 
+void Pacman::Move(char direction) {
+    switch (direction) {
+        case 'w': _position.y -= 1; break;
+        case 's': _position.y += 1; break;
+        case 'a': _position.x -= 1; break;
+        case 'd': _position.x += 1; break;
+    }
+}
+
 std::ostream& operator<<(std::ostream& os, const Pacman& pacman) {
     os << "Pacman: Position = (" << pacman._position.x << ", " << pacman._position.y << "), Lives = " << pacman._lives;
     return os;
