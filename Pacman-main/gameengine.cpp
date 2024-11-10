2788b0872e27eb68d3926d8aec80ac3526bea0f3
eusuntbuga <bugam5160@gmail.com>
2024-11-10 12:53:21 +0200

    Update gameengine.cpp

--- Pacman-main/gameengine.cpp
+++ Pacman-main/gameengine.cpp
@@ -1,22 +1,17 @@
 #include "gameengine.hpp"
+#include <iostream>
+#include <algorithm>
 
-GameEngine::GameEngine() : _pacman(), _board() {
-    // Inițializare fantome
-}
-
-void GameEngine::Init() {
-    // Inițializarea jocului
-}
-
-void GameEngine::Run() {
-    // Loop-ul principal al jocului
-}
-
-bool GameEngine::CheckCollision() {
-    // Verificare coliziune
-    return false;
+GameEngine::GameEngine()
+    : _pacman(std::make_unique<Pacman>()), _board(std::make_unique<Board>()) {
+    for (int i = 0; i < 4; ++i) {
+        _ghosts.push_back(std::make_unique<Ghost>());
+    }
 }
 
 void GameEngine::Display() {
-    // Afișare stare joc
+    std::for_each(_ghosts.begin(), _ghosts.end(), [](const std::unique_ptr<Ghost>& ghost) {
+        std::cout << *ghost << std::endl;
+    });
+    std::cout << *_board << std::endl;
 }
