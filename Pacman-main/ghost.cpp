503b0c10466e3f47fcd35e51bc3e06225b217c6d
eusuntbuga <bugam5160@gmail.com>
2024-11-10 12:53:54 +0200

    Update ghost.cpp

--- Pacman-main/ghost.cpp
+++ Pacman-main/ghost.cpp
@@ -1,24 +1,8 @@
 #include "ghost.hpp"
 #include <iostream>
 
-// Constructor de copiere
-Ghost::Ghost(const Ghost& other) : _position(other._position), _color(other._color) {}
+Ghost::Ghost(const Point& position, const std::string& color) : _position(position), _color(color) {}
 
-// Operator de copiere
-Ghost& Ghost::operator=(const Ghost& other) {
-    if (this != &other) {
-        _position = other._position;
-        _color = other._color;
-    }
-    return *this;
-}
-
-// Operator de comparație
-bool Ghost::operator==(const Ghost& other) const {
-    return _position.x == other._position.x && _position.y == other._position.y && _color == other._color;
-}
-
-// Operator de afișare
 std::ostream& operator<<(std::ostream& os, const Ghost& ghost) {
     os << "Ghost: Position = (" << ghost._position.x << ", " << ghost._position.y << "), Color = " << ghost._color;
     return os;
