19989873696eefa1bcef96cd1c0251727e98e9d5
eusuntbuga <bugam5160@gmail.com>
2024-11-29 21:19:07 +0200

    Update ghost.cpp

--- Pacman-main/ghost.cpp
+++ Pacman-main/ghost.cpp
@@ -1,15 +1,8 @@
 #include "ghost.hpp"
-#include <iostream>
 #include <cstdlib>
 
 Ghost::Ghost(const Point& position, const std::string& color) : _position(position), _color(color) {}
 
-std::ostream& operator<<(std::ostream& os, const Ghost& ghost) {
-    os << "Ghost: Position = (" << ghost._position.x << ", " << ghost._position.y << "), Color = " << ghost._color;
-    return os;
-}
-
-// Functie simpla de deplasare aleatoare a fantomei
 void Ghost::Move() {
     int direction = std::rand() % 4;
     switch (direction) {
@@ -18,4 +11,13 @@ void Ghost::Move() {
         case 2: _position.y += 1; break;
         case 3: _position.y -= 1; break;
     }
+}
+
+Point Ghost::GetPosition() const {
+    return _position;
+}
+
+std::ostream& operator<<(std::ostream& os, const Ghost& ghost) {
+    os << "Ghost: Position = (" << ghost._position.x << ", " << ghost._position.y << "), Color = " << ghost._color;
+    return os;
 }
