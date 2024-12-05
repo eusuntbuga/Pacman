81ee8f9c4dbc13a7e9cd033fe02f7a44aacbf672
eusuntbuga <bugam5160@gmail.com>
2024-12-05 20:52:48 +0200

    Update ghost.hpp

--- Pacman-main/ghost.hpp
+++ Pacman-main/ghost.hpp
@@ -3,6 +3,12 @@
 #include <string>
 #include <iostream>
 
+/**
+ * @class Ghost
+ * @brief Reprezintă un fantomă în joc.
+ * 
+ * Ghost controlează mișcarea fantomei și poziția acesteia pe tabla de joc.
+ */
 class Ghost {
    Point _position;
    std::string _color;
