174fb9ad8f2bbbad61f2e26a5914d0dce19d18b9
eusuntbuga <bugam5160@gmail.com>
2024-12-05 20:50:59 +0200

    Update board.hpp

--- Pacman-main/board.hpp
+++ Pacman-main/board.hpp
@@ -3,18 +3,48 @@
 #include <string>
 #include <iostream>
 
+/**
+ * @class Board
+ * @brief Reprezintă tabla de joc pentru Pac-Man.
+ * 
+ * Board gestionează dimensiunile și conținutul tablei, inclusiv plasarea caracterelor.
+ */
 class Board {
    int _width;
    int _height;
    std::vector<std::vector<char>> _cells;
 
 public:
+   /**
+    * @brief Constructor care inițializează board-ul cu dimensiuni implicite sau date.
+    * @param width Lățimea tablei.
+    * @param height Înălțimea tablei.
+    */
    Board(int width = 28, int height = 31);
+
+   /**
+    * @brief Resetează tabla, umplând-o cu spații goale.
+    */
    void ResetBoard();
+
+   /**
+    * @brief Plasează un caracter pe tabla la poziția specificată.
+    * @param x Coordonata x a poziției.
+    * @param y Coordonata y a poziției.
+    * @param c Caracterul de plasat.
+    */
    void PlaceCharacter(int x, int y, char c);
 
-   // Getter
+   /**
+    * @brief Obține lățimea tablei.
+    * @return Lățimea tablei.
+    */
    int GetWidth() const;
+
+   /**
+    * @brief Obține înălțimea tablei.
+    * @return Înălțimea tablei.
+    */
    int GetHeight() const;
 
    friend std::ostream& operator<<(std::ostream& os, const Board& board);
