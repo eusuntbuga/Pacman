10388ae1dfb44f79a2286a1159bd32a1947a806c
eusuntbuga <bugam5160@gmail.com>
2024-12-05 21:01:15 +0200

    Update pacman.hpp

--- Pacman-main/pacman.hpp
+++ Pacman-main/pacman.hpp
@@ -1,21 +1,52 @@
+/**
+ * @file pacman.hpp
+ * @brief Header file for Pacman class.
+ * 
+ * @project PacMan Game
+ * @author Mihai
+ * @year 2024
+ * 
+ * @description
+ * This file contains the definition of the Pacman class, which manages
+ * the movement and state of Pacman in the game.
+ */
+
 #pragma once
 #include "point.hpp"
 
-/**
- * @class Pacman
- * @brief Reprezintă personajul principal, Pac-Man.
- * 
- * Pacman controlează poziția și mișcarea acestuia pe tabla de joc.
- */
 class Pacman {
-   Point _position;
-   int _lives;
+   Point _position; ///< Current position of Pacman on the board.
+   int _lives; ///< Number of lives remaining for Pacman.
 
 public:
-   Pacman(int x = 1, int y = 1);
+   /**
+    * @brief Construct a new Pacman object.
+    * Initializes the position of Pacman and sets the number of lives to 3.
+    */
+   Pacman();
+
+   /**
+    * @brief Move Pacman in the specified direction.
+    * @param direction Direction for movement ('w', 'a', 's', 'd').
+    */
    void Move(char direction);
-   void LoseLife();
+
+   /**
+    * @brief Get the current position of Pacman.
+    * @return Point Current position of Pacman.
+    */
    Point GetPosition() const;
-   int GetLives() const;
-   void SetPosition(int x, int y);
+
+   /**
+    * @brief Decrease Pacman's lives by 1.
+    */
+   void LoseLife();
+
+   /**
+    * @brief Overload of the output stream operator to display Pacman's state.
+    * @param os Output stream.
+    * @param pacman Pacman object to be displayed.
+    * @return std::ostream& Output stream with Pacman's information.
+    */
+   friend std::ostream& operator<<(std::ostream& os, const Pacman& pacman);
 };
