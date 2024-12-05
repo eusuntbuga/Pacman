ef39f5759d5b779da2e7fa73c168334037e767cd
eusuntbuga <bugam5160@gmail.com>
2024-12-05 20:54:37 +0200

    Update pacman.cpp

--- Pacman-main/pacman.cpp
+++ Pacman-main/pacman.cpp
@@ -1,25 +1,30 @@
 #include "pacman.hpp"
 
-Pacman::Pacman() : _position{0, 0}, _lives(3) {}
+Pacman::Pacman(int x, int y) : _position(x, y), _lives(3) {}
 
 void Pacman::Move(char direction) {
-    switch (direction) {
-        case 'w': _position.y -= 1; break;
-        case 's': _position.y += 1; break;
-        case 'a': _position.x -= 1; break;
-        case 'd': _position.x += 1; break;
-    }
-}
-
-Point Pacman::GetPosition() const {
-    return _position;
+   switch (direction) {
+       case 'W': _position.y -= 1; break;  // Muta în sus
+       case 'S': _position.y += 1; break;  // Muta în jos
+       case 'A': _position.x -= 1; break;  // Muta la stânga
+       case 'D': _position.x += 1; break;  // Muta la dreapta
+   }
 }
 
 void Pacman::LoseLife() {
-    if (_lives > 0) --_lives;
+   if (_lives > 0) {
+       --_lives;
+   }
 }
 
-std::ostream& operator<<(std::ostream& os, const Pacman& pacman) {
-    os << "Pacman: Position = (" << pacman._position.x << ", " << pacman._position.y << "), Lives = " << pacman._lives;
-    return os;
+Point Pacman::GetPosition() const {
+   return _position;
+}
+
+int Pacman::GetLives() const {
+   return _lives;
+}
+
+void Pacman::SetPosition(int x, int y) {
+   _position = Point{x, y};
 }
